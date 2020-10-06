cabdc2967aa8d7cced5cfb8093ed7d1d5d65527a
Paul Sinnett <paul.sinnett@gmail.com>
2020-10-06 14:11:51 +0100

    example raycast hit detection

---
+++ Assets/Raycast.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public float distance = 20.0f;
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
+        {
+            if (hit.collider.CompareTag("GrappleTarget"))
+            {
+                Debug.Log("Hit!");
+            }
+        }
+    }
+}
--- journal.md
+++ journal.md
@@ -3,3 +3,5 @@
 ## 06/10/2020
 
 GitHub includes the spreadsheet lock file by default. I removed it from the commit.
+
+I had a problem with the `HasTag` function, it should be `CompareTag`.
